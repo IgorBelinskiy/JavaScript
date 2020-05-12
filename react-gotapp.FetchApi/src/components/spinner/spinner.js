0e5b34969ae6a2d4f64bc0fc8a0c9a9ad49531ec
Igor <i.belinskiy92@gmail.com>
2020-05-12 23:52:25 +0300

    Работа с API

---
+++ react-gotapp.FetchApi/src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './spinner.css'
+
+
+const Spinner = () => {
+   return (
+      <div class="loadingio-spinner-spinner-3xcn3sf2lm"><div class="ldio-xy2odttkcj">
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div></div>
+   )
+}
+
+
+export default Spinner;
+
+
+
