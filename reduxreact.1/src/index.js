ab5ddf2348d8ebe41d245a30a1c3518f254d6b82
Igor <i.belinskiy92@gmail.com>
2020-05-13 21:47:13 +0300

    Основные принципы Redux

---
+++ reduxreact.1/src/index.js
@@ -0,0 +1,57 @@
+import {createStore} from 'redux';
+
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RND':
+      return state + action.value;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer)
+
+// Вспомогательные функции
+const inc = () => ( {type: 'INC'} );
+const dec = () => ( {type: 'DEC'} );
+const rnd = (value) => ( {type: 'RND', value} );
+
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc())
+})
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec())
+})
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10)
+  store.dispatch(rnd(value))
+})
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState()
+}
+
+store.subscribe(update)
+// store.subscribe(() => {
+//   console.log(store.getState())
+// })
+
+// store.dispatch({type: 'INC'})
+// console.log(store.getState())
+// store.dispatch({type: 'INC'})
+// console.log(store.getState())
+// store.dispatch({type: 'INC'})
+
+// let state = reducer(undefined, {})
+
+// state = reducer(state, {type: 'INC'})
+// console.log(state)
+// state = reducer(state, {type: 'INC'})
+// console.log(state)
